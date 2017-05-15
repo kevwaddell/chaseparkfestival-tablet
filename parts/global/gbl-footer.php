e017ea9f71f8db250ea2de26fd66384a5997ec6a
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-05-15 16:56:26 +0100

    Update location and ticket button to be active and inactive.

--- parts/global/gbl-footer.php
+++ parts/global/gbl-footer.php
@@ -1,12 +1,13 @@
 <?php  
 /* FESTIVAL LOCATION AND DATE DATA */
+$location_active = get_field('cpf_date_active', 'options');
 $fest_location = get_field('cpf_location', 'options');
 $fest_date = get_field('cpf_date', 'options');
 $fest_time = get_field('cpf_time', 'options');	
 	
 /* TICKET DATA */
+$tickets_active = get_field('tickets_active', 'options');
 $ticket_provider_http = get_field('gbl_ticket_provider', 'options');
-$ticket_provider = preg_split("http://", $ticket_provider_http);
 $tickets_url = get_field('gbl_tickets_url', 'options');
 
 /* CONTACT DATA */
@@ -37,23 +38,26 @@ $social_links = get_field('gbl_social_links', 'options');
 					?>
 				</div>
 			</div>
-			
+			<?php if ($tickets_active) { ?>
 			<div class="ticket-details">
 				<div class="ticket-info text-center">
 					Tickets are available for purchase from<br />
-					<a href="<?php echo $ticket_provider_http; ?>"><?php echo $ticket_provider[1]; ?></a>
+					<a href="<?php echo $ticket_provider_http; ?>"><?php echo str_replace("http://", "", $ticket_provider_http); ?></a>
 				</div>
 				<a href="<?php echo $tickets_url; ?>" target="_blank" class="btn btn-default btn-block book-tickets-btn tk-azo-sans-uber">Book Your Tickets</a>
 			</div>
-
+			<?php } ?>
+			
 			<div class="contact-info text-center txt-col-blue-dk"><strong>For more Information contact <?php echo $contact_name; ?> on <a href="tel:<?php echo $contact_tel_link; ?>"><?php echo $contact_tel; ?></a> or <a href="mailto:<?php echo $contact_email; ?>"><?php echo $contact_email; ?></a></strong></div>
 			</div>
 		
+		<?php if ($location_active) { ?>
 		<div class="date-location clearfix tk-azo-sans-uber txt-col-wht">
 			<time class="footer-date dl-item bg-col-blue pull-left"><?php echo $fest_date; ?></time>
 			<div class="footer-location dl-item bg-col-orange in-block pull-left"><?php echo $fest_location; ?></div>
 			<time class="footer-time dl-item bg-col-blue-dk in-block pull-right"><?php echo $fest_time; ?></time>
 		</div>
+		<?php } ?>
 		
 		<?php if (!empty($social_links)) { ?>
 		<div class="social-links">
--- parts/homepage/hp-masthead.php
+++ parts/homepage/hp-masthead.php
@@ -1,8 +1,10 @@
 <?php  
 /* FESTIVAL LOCATION AND DATE DATA */
+$location_active = get_field('cpf_date_active', 'options');
 $fest_location = get_field('cpf_location', 'options');
 $fest_date = get_field('cpf_date', 'options');
 $fest_time = get_field('cpf_time', 'options');	
+$tickets_active = get_field('tickets_active', 'options');
 $gbl_tickets_url = get_field('gbl_tickets_url', 'options');
 ?>
 
@@ -10,12 +12,18 @@ $gbl_tickets_url = get_field('gbl_tickets_url', 'options');
 	<div class="mast-bg-stripes"></div>
 	
 	<div class="container-fluid">
-		
+			<?php if ($location_active) { ?>
 			<div class="date-location text-center tk-azo-sans-uber">
 				<time class="hp-mast-date dl-item"><?php echo $fest_date; ?></time>
 				<div class="hp-mast-location dl-item"><?php echo $fest_location; ?></div>
 				<time class="hp-mast-time dl-item"><?php echo $fest_time; ?></time>
 			</div>
+			<?php } else { 
+			$gbl_future_message = get_field('gbl_future_message', 'options');	
+			?>
+			<div class="hp-mast-message text-center tk-azo-sans-uber"><?php echo $gbl_future_message; ?></div>
+			<?php } ?>
+
 			
 			<div class="logo-wrap">
 				<div class="logo"><span class="sr-only"><?php bloginfo('name'); ?></span></div>
@@ -26,7 +34,9 @@ $gbl_tickets_url = get_field('gbl_tickets_url', 'options');
 			
 			<button id="main-nav-btn" class="btn btn-default btn-block"><span class="menu-label txt-col-wht tk-azo-sans-uber">Menu</span><i class="fa fa-bars fa-lg"></i></button>
 		
+		<?php if ($tickets_active) { ?>
 		<a href="<?php echo $gbl_tickets_url; ?>" target="_blank" class="btn btn-default btn-block btn-lg book-tickets-btn-lg tk-azo-sans-uber">Book Your Tickets</a>
+		<?php } ?>
 		
 	</div>
 	
