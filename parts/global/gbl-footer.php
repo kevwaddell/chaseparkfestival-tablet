34efd686d975e3261d3c8014b0e2a299320ad4dc
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-03-20 14:18:00 +0000

    New styles for 2018

--- parts/global/gbl-footer.php
+++ parts/global/gbl-footer.php
@@ -24,7 +24,7 @@ $social_links = get_field('gbl_social_links', 'options');
 		
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-8 col-md-offset-2">
+				<div class="col-xs-11 col-xs-offset-1 col-md-10 col-md-offset-1">
 					<?php wp_nav_menu(array( 
 						'container' => 'false', 
 						'menu' => 'Footer Main Navigation', 
